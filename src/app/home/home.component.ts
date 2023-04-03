983edb72b38391ec8200141c720198220d126057
Ashu Raut <ashu.raut@nagarro.com>
2023-04-03 17:40:03 +0530

    first commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,18 +8,20 @@ import { OktaAuth } from '@okta/okta-auth-js';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent  {
 
   constructor(@Inject(OKTA_AUTH) public oktaAuth: OktaAuth, public authService: OktaAuthStateService,private router: Router,) {
-  }
-  ngOnInit(): void {
     this.authService.authState$.subscribe(x=>{
       if(x.isAuthenticated){
-       //this.router.navigate(['planning']);
-       this.oktaAuth.signInWithRedirect()
+       this.router.navigate(['planning']);
+       //this.oktaAuth.signInWithRedirect()
+      }
+      else{
+        this.oktaAuth.signInWithRedirect()
       }
     })
   }
+ 
   seave(){
     console.log(this.oktaAuth.getAccessToken());
   }
